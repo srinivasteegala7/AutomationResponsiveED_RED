e21fd9821ecbf5e5c16c4d119cda3138591a66a9
Srinivas Teegala <srinivas.t@sapphirus.in>
2024-02-11 19:01:20 +0530

    RED

---
+++ src/main/java/com/cucumber/PageObjects/allUserManagementDropDwnHandlePageObj.java
@@ -0,0 +1,82 @@
+package com.cucumber.PageObjects;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import com.cucumber.TestBase.TestBase;
+import com.cucumber.utility.Constants;
+
+
+public class allUserManagementDropDwnHandlePageObj {
+	WebDriver driver;
+	Actions act;
+	TestBase testbase;
+
+	By userMenu = By.xpath("//span[text()='Users']/../../../..");
+	By institutinsDropDown = By.xpath("//mat-label[text()='Institutions:']/../../../..");
+	By instSelect =By.xpath("//span[text()='ResponsiveEd Institutions']");
+	By rolesDropDown = By.xpath("//mat-label[text()=\"Role's:\"]/../../../..");
+	By roleSelect = By.xpath("(//span[text()='Administrator'])[2]");
+	By enrollmentStatusDropDown = By.xpath("//mat-label[text()='Enrollment Status:']/../../../..");
+	By statusSelect = By.xpath("(//span[text()='All'])[2]");
+	By searchByDropDown = By.xpath("//mat-label[text()='Search By:']/../../../..");
+	By searchSelect = By.xpath("(//span[text()='All'])[3]");
+    By goButton = By.xpath("//span[text()='GO']");
+
+
+    public allUserManagementDropDwnHandlePageObj(WebDriver driver) {
+		this.driver = driver;
+		testbase = new TestBase();
+	}
+
+
+	public void ClickontheUSERMENU() throws Exception {
+		Constants.driver.findElement(userMenu).click();
+		Thread.sleep(2000);;
+	}
+
+
+	public void SelectinstitutinsDropDown() throws Exception {
+		Constants.driver.findElement(institutinsDropDown).click();
+		Thread.sleep(2000);
+		Constants.driver.findElement(instSelect).click();
+		Thread.sleep(2000);
+	}
+
+
+	public void SelectrolesDropDown() throws Exception {
+		Constants.driver.findElement(rolesDropDown).click();
+		Thread.sleep(2000);
+		Constants.driver.findElement(roleSelect).click();
+		Thread.sleep(2000);
+		
+	}
+
+
+	public void SelectenrollmentStatusDropDown() throws Exception {
+		Constants.driver.findElement(enrollmentStatusDropDown).click();
+		Thread.sleep(2000);
+		Constants.driver.findElement(statusSelect).click();
+		Thread.sleep(2000);
+	}
+
+
+	public void SlectsearchByDropDown() throws Exception {
+		Constants.driver.findElement(searchByDropDown).click();
+		Thread.sleep(2000);
+		Constants.driver.findElement(searchSelect).click();
+		Thread.sleep(2000);
+	}
+
+
+	public void ClickONGOBUTTON() throws InterruptedException {
+		Constants.driver.findElement(goButton).click();
+		Thread.sleep(4000);
+	}
+
+
+	
+    
+
+}
---
+++ target/cucumber-pretty/report.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/SearchUser.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Administrator - Search User",
+  "description": "",
+  "id": "administrator---search-user",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@tag"
+    }
+  ]
+});
+formatter.scenario({
+  "line": 5,
+  "name": "User Management Search User",
+  "description": "",
+  "id": "administrator---search-user;user-management-search-user",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 4,
+      "name": "@sanity"
+    }
+  ]
+});
+formatter.step({
+  "line": 6,
+  "name": "User on the loginpage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "User Click on the UserMenu",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "User selected All Drop Down Values",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "User Clicks GO button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User Enter User Nane And search",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "searchUserStepDef.user_on_the_loginpage()"
+});
+formatter.result({
+  "duration": 10550741500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "searchUserStepDef.user_Click_on_the_UserMenu()"
+});
+formatter.result({
+  "duration": 2186721000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "searchUserStepDef.user_selected_All_Drop_Down_Values()"
+});
+formatter.result({
+  "duration": 25091533700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "searchUserStepDef.user_Clicks_GO_button()"
+});
+formatter.result({
+  "duration": 4150002100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "searchUserStepDef.user_Enter_User_Nane_And_search()"
+});
+formatter.result({
+  "duration": 14654530100,
+  "status": "passed"
+});
+});
